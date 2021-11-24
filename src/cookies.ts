965a6284b19cabd2162b12793ae1e5b9a5d69836
Marko Klemetti <marko.klemetti@gmail.com>
2021-11-24 20:16:16 +0200

    refactor(app): refactor cookie to plural

--- src/cookies.ts
+++ src/cookies.ts
@@ -2,7 +2,7 @@ import { getCookies } from 'chrome-cookies-secure';
 
 export const getCookiesFromBrowser = async () => {
   return new Promise((resolve, reject) => {
-    getCookies('https://timesheets.eficode.fi/api/employees/me', (err: any, data: any) => {
+    getCookies('https://timesheets.eficode.fi/api/employees/me', (err: any, data: string) => {
       err ? reject(err) : resolve(data);
     });
   });
