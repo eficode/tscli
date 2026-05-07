a6de8b49771fb5b7024bbe422b3acdb303b801e1
Ahmed Magdy <a-magdy@users.noreply.github.com>
2026-05-07 08:02:33 +0200

    chore: update getCookies call to include options and move CHROME_PROFILE declaration

--- src/cookies.ts
+++ src/cookies.ts
@@ -5,15 +5,24 @@ const combineCookieString = (data: any) => {
     .map((key: any) => `${key}=${data[key]}`)
     .join('; ');
 
-    return cookies;
+  return cookies;
 };
+
+const CHROME_PROFILE = process.env.CHROME_PROFILE || 'Default';
 
 export const getCookiesFromBrowser = async () => {
   return new Promise((resolve, reject) => {
-    getCookies('https://timesheets.eficode.fi', (err: any, data: any) => {
-      if (err) { reject(err); }
+    getCookies(
+      'https://timesheets.eficode.fi',
+      'object' as any,
+      (err: any, data: any) => {
+        if (err) {
+          reject(err);
+        }
 
-      resolve(combineCookieString(data));
-    });
+        resolve(combineCookieString(data));
+      },
+      CHROME_PROFILE,
+    );
   });
 };
